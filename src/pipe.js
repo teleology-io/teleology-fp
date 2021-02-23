9f215e649c74e0ec447d9f8260a1e5138a26daaa
Chris Sullivan <chrissullivan.dev@gmail.com>
2021-02-22 21:17:54 -0800

    F

--- src/pipe.js
+++ src/pipe.js
@@ -1,6 +1,6 @@
 export const isAsyncFunction = (v) =>
   (v && v.then && typeof v.then === 'function') ||
-  v[Symbol.toStringTag] === 'AsyncFunction';
+  (v && v[Symbol.toStringTag] === 'AsyncFunction');
 
 export const pipe = (...fn) => (initial) =>
   fn.reduce((a, n) => (isAsyncFunction(n) ? n.then(a) : n(a)), initial);
