c7b9b414172b84409e83428098f2b292c6b1c8dd
Chris Sullivan <chrissullivan.dev@gmail.com>
2021-06-04 10:09:54 -0700

    Adding rolloff to the retry method

--- src/retry.js
+++ src/retry.js
@@ -1,12 +1,19 @@
-export const retry = (fn, max = 3) => async (...args) => {
-  for (let i = max - 1; i > -1; i -= 1) {
+/* eslint-disable no-await-in-loop */
+
+const DEFAULT_ROLLOFF = async () => {};
+
+export const retry = (fn, max = 3, rolloff = DEFAULT_ROLLOFF) => async (
+  ...args
+) => {
+  for (let i = 0; i < max; i += 1) {
     try {
-      // eslint-disable-next-line no-await-in-loop
       return await fn(...args);
     } catch (e) {
-      if (!i) {
+      if (i + 1 >= max) {
         throw e;
       }
+
+      await rolloff(i + 1);
     }
   }
 };
